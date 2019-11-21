efb53c159b6285cd2a87a6427d2d3dbd5ff7e192
cantona <cantona642@gmail.com>
2019-11-22 00:33:59 +0700

    Tugas Akhir

---
+++ android/app/src/main/kotlin/com/tugasakhir/remake_ta/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.tugasakhir.remake_ta
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/Homepage.dart
@@ -0,0 +1,345 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+//import 'package:permission_handler/permission_handler.dart';
+import 'map_view.dart';
+
+const API_KEY = "AIzaSyDp-x54CkbpwRxOn4g-IALcuGC7YFbpOhU";
+
+class HomePage extends StatefulWidget{
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage>{
+  Completer<GoogleMapController> _controller = Completer();
+  MapView mapView = new MapView();
+
+  @override
+  void main(){
+    MapView.setApiKey(API_KEY);
+  }
+
+  double zoomVal=5.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(FontAwesomeIcons.arrowLeft),
+            onPressed: () {
+              //
+            }),
+        title: Text("Museum"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(FontAwesomeIcons.search),
+              onPressed: () {
+                //
+              }),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          _buildGoogleMap(context),
+          SizedBox(child: _zoomminusfunction()),
+          SizedBox(child: _zoomplusfunction()),
+          _buildContainer(),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _zoomminusfunction() {
+
+    return Align(
+      alignment: Alignment.centerRight,
+      child: IconButton(
+          icon: Icon(FontAwesomeIcons.searchMinus,color:Color(0xff6200ee)),
+          onPressed: () {
+            zoomVal--;
+            _minus(zoomVal);
+          }),
+    );
+  }
+  Widget _zoomplusfunction() {
+
+
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: IconButton(
+          icon: Icon(FontAwesomeIcons.searchPlus,color:Color(0xff6200ee)),
+          onPressed: () {
+            zoomVal++;
+            _plus(zoomVal);
+          }),
+    );
+  }
+
+  Future<void> _minus(double zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(-6.1753924, 106.8249641), zoom: zoomVal)));
+  }
+  Future<void> _plus(double zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(-6.1753924, 106.8249641), zoom: zoomVal)));
+  }
+
+
+  Widget _buildContainer() {
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 20.0),
+        height: 150.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://upload.wikimedia.org/wikipedia/commons/4/45/Museum_Nasional_Indonesia.jpg",
+                  -6.1764021, 106.8194014,"Museum Nasional"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://lh6.googleusercontent.com/proxy/NcYZsJMPq4Tzn5aFEk0IBGw2zMzFA4Zut9-EmAdXOP4QKALlKSHzCX8A_7Ud_ahAsxbffvKIusGwEfAI-JLyjaOglhu_1-JRavnk9Sfo4GMzS9DawipmJV_tSalMOoYsN2IvoiZHOykWBXZaiBaif259_vOV8O0=w408-h272-k-no",
+                  -6.1352, 106.8111113,"Museum Sejarah Jakarta (Fatahilah)"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://images.unsplash.com/photo-1504940892017-d23b9053d5d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                  40.732128, -73.999619,"Blue Hill"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _boxes(String _image, double lat,double long,String namaMuseum) {
+    return  GestureDetector(
+      onTap: () {
+        _gotoLocation(lat,long);
+      },
+      child:Container(
+        child: new FittedBox(
+          child: Material(
+              color: Colors.white,
+              elevation: 14.0,
+              borderRadius: BorderRadius.circular(24.0),
+              shadowColor: Color(0x802196F3),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    width: 180,
+                    height: 200,
+                    child: ClipRRect(
+                      borderRadius: new BorderRadius.circular(24.0),
+                      child: Image(
+                        fit: BoxFit.fill,
+                        image: NetworkImage(_image),
+                      ),
+                    ),),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: myDetailsContainer1(namaMuseum),
+                    ),
+                  ),
+
+                ],)
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget myDetailsContainer1(String namaMuseum) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Container(
+              child: Text(namaMuseum,
+                style: TextStyle(
+                    color: Color(0xff6200ee),
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold),
+              )),
+        ),
+        SizedBox(height:5.0),
+        Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                    child: Text(
+                      "4.1",
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 18.0,
+                      ),
+                    )),
+                Container(
+                  child: Icon(
+                    FontAwesomeIcons.solidStar,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                  child: Icon(
+                    FontAwesomeIcons.solidStar,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                  child: Icon(
+                    FontAwesomeIcons.solidStar,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                  child: Icon(
+                    FontAwesomeIcons.solidStar,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                  child: Icon(
+                    FontAwesomeIcons.solidStarHalf,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                    child: Text(
+                      "(946)",
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 18.0,
+                      ),
+                    )),
+              ],
+            )),
+        SizedBox(height:5.0),
+        Container(
+            child: Text(
+              "Jakarta \u00B7 \u0024\u0024 \u00B7",
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 18.0,
+              ),
+            )),
+        SizedBox(height:5.0),
+        Container(
+            child: Text(
+              "Closed \u00B7 Opens 17:00 Thu",
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            )),
+      ],
+    );
+  }
+
+  Widget _buildGoogleMap(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
+        mapType: MapType.normal,
+        myLocationEnabled: true,
+        initialCameraPosition:  CameraPosition(target: LatLng(-6.1753924, 106.8249641), zoom: 12),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+
+        markers: {
+          newyork1Marker,newyork2Marker,newyork3Marker, museumnasionalMarker, fatahilahMarker,blueMarker,mymarker
+        },
+      ),
+    );
+  }
+
+  Future<void> _gotoLocation(double lat,double long) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(lat, long), zoom: 15,tilt: 50.0,
+      bearing: 45.0,),
+      ),
+    );
+  }
+}
+Marker mymarker = Marker(
+  markerId: MarkerId('markersaya'),
+);
+
+Marker museumnasionalMarker = Marker(
+  draggable: false,
+  markerId: MarkerId('museumNasional'),
+  position: LatLng(-6.1764021, 106.8194014),
+  infoWindow: InfoWindow(title: 'Museum Nasional'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueOrange,
+  ),
+);
+
+Marker fatahilahMarker = Marker(
+  draggable: false,
+  markerId: MarkerId('fatahilah'),
+  position: LatLng(-6.1352, 106.8111113),
+  infoWindow: InfoWindow(title: 'Museum Sejarah Jakarta'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueOrange,
+  ),
+);
+Marker blueMarker = Marker(
+  draggable: false,
+  markerId: MarkerId('bluehill'),
+  position: LatLng(40.732128, -73.999619),
+  infoWindow: InfoWindow(title: 'Blue Hill'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+
+//New York Marker
+
+Marker newyork1Marker = Marker(
+  markerId: MarkerId('newyork1'),
+  position: LatLng(40.742451, -74.005959),
+  infoWindow: InfoWindow(title: 'Los Tacos'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+Marker newyork2Marker = Marker(
+  markerId: MarkerId('newyork2'),
+  position: LatLng(40.729640, -73.983510),
+  infoWindow: InfoWindow(title: 'Tree Bistro'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+Marker newyork3Marker = Marker(
+  markerId: MarkerId('newyork3'),
+  position: LatLng(40.719109, -74.000183),
+  infoWindow: InfoWindow(title: 'Le Coucou'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
